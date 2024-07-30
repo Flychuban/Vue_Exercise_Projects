2138b251ecb36998f85831e4dc610fa4f82e5719
Flychuban <kaloanas07@gmail.com>
2024-07-30 14:59:48 +0300

    add code for basic gmap

--- gmaps_nuxt/app.vue
+++ gmaps_nuxt/app.vue
@@ -1,5 +1,45 @@
+<script setup>
+
+import { ref, onMounted } from 'vue'
+import { Loader } from "@googlemaps/js-api-loader"
+
+
+const loader = new Loader({
+  apiKey: "AIzaSyDOc0GjZZ2KF5rzCyW2V8t7Ozxvmz553Lk",
+})
+
+const mapDiv = ref(null)
+
+onMounted(async () => {
+  await loader.importLibrary('maps')
+  .then(
+    ({Map}) => {
+      new Map(mapDiv.value, {
+        center: { lat: 42.69, lng: 23.32 },
+        zoom: 11,
+        gestureHandling: "cooperative",
+        mapTypeId: 'roadmap'
+      });
+    }
+  )
+})
+
+</script>
+
 <template>
-  <div>
-    <h1>Hello world</h1>
-  </div>
+  <h1>Maps</h1>
+  <div ref="mapDiv" class="map-container"></div>
 </template>
+
+<style scoped>
+
+h1 {
+  text-align: center;
+}
+.map-container {
+  width: 90dvw;
+  height: 90dvh;
+  margin: auto;
+}
+
+</style>
